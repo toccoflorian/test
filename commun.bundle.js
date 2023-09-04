02ab5f1a88a1e2a8ba1eb926e643928c97ee98ac
Florian <t.florian181181@gmail.com>
2023-09-04 16:41:19 +0200

    topbar

--- commun.bundle.js
+++ commun.bundle.js
@@ -9,11 +9,29 @@ headerlogoElement.addEventListener('click', event => {
   location = './index.html';
 });
 
-// const scrollDocumentElement = document.addEventListener('scroll', event => {
-//     const headerElement = document.querySelector('header');
-//     headerElement.classList.add('scroll');
-//     console.log('scroll');
-// })
+// detection du scroll vers le bas
+window.addEventListener('DOMContentLoaded', event => {
+  const headerElement = document.querySelector('header');
+  const DScrollElement = document.querySelector('.down-scroll');
+  const contentElement = document.querySelector('.content');
+  let lastScrollY;
+  document.addEventListener('scroll', event => {
+    if (scrollY > 0) {
+      headerElement.classList.add('down-scroll');
+      contentElement.style = 'margin-top: 120px';
+    } else {
+      headerElement.classList.remove('down-scroll');
+      contentElement.style = 'margin-top: 0px';
+    }
+    if (scrollY > lastScrollY) {
+      DScrollElement.style.top = '0px';
+      console.log(DScrollElement);
+    } else if (scrollY < lastScrollY) {
+      DScrollElement.style.top = '-120px';
+    }
+    lastScrollY = scrollY;
+  });
+});
 /******/ })()
 ;
 //# sourceMappingURL=commun.bundle.js.map
